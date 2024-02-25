e6c42ca49439541d8e2d49a3ae872ede15a9342b
tiffany <21ttran11@gmail.com>
2024-02-25 02:37:01 -0800

    mm

--- environCo-Inspector/Assets/Scripts/Recycler.cs
+++ environCo-Inspector/Assets/Scripts/Recycler.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Recycler : MonoBehaviour
 {
     [SerializeField] bool isRecycler;
+    public static ItemSpawner instance;
+    [SerializeField] public float spawned;
 
     void OnCollisionEnter2D(Collision2D col)
     {   // Recycle layer = 10
         if (col.collider.gameObject.layer == 10 && isRecycler )
         {
             Destroy(col.gameObject);
+            spawned -= 1;
         }
 
         else if (col.collider.gameObject.layer == 11 && !isRecycler)
         {
             Destroy(col.gameObject);
+            spawned -= 1;
+        }
+    }
+
+    private void Update()
+    {
+        if (spawned == 0)
+        {
+            SceneManager.LoadScene("MiniGame2");
         }
     }
 }
