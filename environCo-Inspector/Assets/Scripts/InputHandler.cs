9d9c55c7d09fb3198b09837884c532afec6ef95a
Josue Cota <xJaramis@gmail.com>
2024-02-24 17:18:50 -0800

    Added InputHandler

---
+++ environCo-Inspector/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    private Camera mainCamera;
+    private GameObject selectedObject;
+
+    private Vector2 offSet;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            var rayHit = Physics2D.GetRayIntersection(mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+
+            if (rayHit.collider)
+            {
+                Debug.Log(rayHit.collider.gameObject.name);
+                selectedObject = rayHit.collider.gameObject;
+                offSet = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)selectedObject.transform.position;
+            }
+        }
+        else if (context.canceled)
+        {
+            selectedObject = null;
+            Debug.Log("Stopped click");
+        }
+    }
+
+    public void Drag()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        selectedObject.transform.position = mousePosition - offSet;
+        Debug.Log("Dragging");
+    }
+
+    public void Update()
+    {
+        if (selectedObject != null)
+        {
+            Drag();
+        }
+    }
+}
